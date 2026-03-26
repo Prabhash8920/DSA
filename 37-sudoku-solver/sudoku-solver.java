9d4c26724f5b960060ad91da18833b420002b615
Prabhash kumar <Prabhashkumar7835@gmail.com>
2026-03-26 15:58:34 +0530

    Time: 173 ms (75.82%) | Memory: 42.1 MB (99.53%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    public boolean isValid(char[][] board, int row, int col, char num) {
+
+        // check row
+        for(int j = 0; j < 9; j++) {
+            if(board[row][j] == num) return false;
+        }
+
+        // check column
+        for(int i = 0; i < 9; i++) {
+            if(board[i][col] == num) return false;
+        }
+
+        // check 3x3 grid
+        int sRow = (row / 3) * 3;
+        int sCol = (col / 3) * 3;
+
+        for(int i = sRow; i < sRow + 3; i++) {
+            for(int j = sCol; j < sCol + 3; j++) {
+                if(board[i][j] == num) return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean solve(char[][] board, int row, int col) {
+
+        if(row == 9) return true;
+
+        if(col == 9) return solve(board, row + 1, 0);
+
+        if(board[row][col] != '.') {
+            return solve(board, row, col + 1);
+        }
+
+        for(char ch = '1'; ch <= '9'; ch++) {
+            if(isValid(board, row, col, ch)) {
+                board[row][col] = ch;
+
+                if(solve(board, row, col + 1)) return true;
+
+                board[row][col] = '.'; // backtrack
+            }
+        }
+
+        return false;
+    }
+
+    public void solveSudoku(char[][] board) {
+        solve(board, 0, 0);
+    }
+}
