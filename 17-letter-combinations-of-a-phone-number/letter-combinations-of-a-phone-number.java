fb597d23e614b7ccefe0276907eb4f76d915b93a
Prabhash kumar <Prabhashkumar7835@gmail.com>
2026-03-29 15:25:14 +0530

    Time: 3 ms (34.14%) | Memory: 49 MB (22.01%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
+        if (digits == null || digits.length() == 0) return ans;
+
+        Map<Character, String> map = getMap();
+        solve(0, digits, "", ans, map);
+        return ans;
+    }
+
+    private void solve(int i, String digits, String current, List<String> ans, Map<Character, String> map) {
+        // Base case
+        if (i == digits.length()) {
+            ans.add(current);
+            return;
+        }
+
+        // Get letters corresponding to current digit
+        String letters = map.get(digits.charAt(i));
+
+        for (char c : letters.toCharArray()) {
+            solve(i + 1, digits, current + c, ans, map);
+        }
+    }
+
+    private Map<Character, String> getMap() {
+        Map<Character, String> map = new HashMap<>();
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+        return map;
+    }
+}
