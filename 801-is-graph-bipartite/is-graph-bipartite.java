4e166058a3f49715f9c732d495d2d498a34d8f4f
Prabhash kumar <Prabhashkumar7835@gmail.com>
2026-07-19 17:09:39 +0530

    Time: 1 ms (61.09%) | Memory: 46.9 MB (90.30%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,43 +1,35 @@
 import java.util.*;
-
 class Solution {
-
-    public boolean isBipartite(int[][] graph) {
-        int n = graph.length;
-        int[] color = new int[n];
-        Arrays.fill(color, -1);
-
-        for (int i = 0; i < n; i++) {
-            if (color[i] == -1) {
-                if (!bfs(i, graph, color))
-                    return false;
-            }
-        }
-
-        return true;
-    }
-
-    private boolean bfs(int start, int[][] graph, int[] color) {
+    static boolean ans;
+    public void bfs(int i, int[][] adj, int[] visited){
         Queue<Integer> q = new LinkedList<>();
-
-        q.offer(start);
-        color[start] = 0;
-
-        while (!q.isEmpty()) {
-            int node = q.poll();
-
-            for (int neighbor : graph[node]) {
-
-                if (color[neighbor] == -1) {
-                    color[neighbor] = 1 - color[node];
-                    q.offer(neighbor);
-                } 
-                else if (color[neighbor] == color[node]) {
-                    return false;
+        visited[i] =0;
+        q.add(i);
+        while(q.size()>0){
+            int front = q.remove();
+            int color = visited[front];
+            for(int ele : adj[front]){
+                if(visited[ele]==visited[front]){
+                    ans = false;
+                    return;
+                }
+                if(visited[ele]==-1){
+                    visited[ele] = 1-color;
+                    q.add(ele);
                 }
             }
         }
-
-        return true;
+    }
+    public boolean isBipartite(int[][] adj) {
+        ans = true;
+        int n = adj.length;
+        int [] visited = new int[n];
+        Arrays.fill(visited,-1);
+        for(int i =0; i<n; i++){
+            if(ans==false) return ans;
+            if(visited[i]==-1) bfs(i,adj,visited);
+        }
+        return ans;
+    
     }
 }
