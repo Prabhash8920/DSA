5b5fa7c8b3a547c709a952e948a9c305db8edff8
Prabhash kumar <Prabhashkumar7835@gmail.com>
2026-07-25 19:23:54 +0530

    Time: 4 ms (91.53%) | Memory: 46.9 MB (78.66%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,32 +1,33 @@
 class Solution {
+    static boolean ans;
+    public void dfs(int i, List<List<Integer>> adj,boolean[]vis,boolean[] path){
+        vis[i] = true;
+        path[i] = true;
+        for(int ele:adj.get(i)){
+            if(path[ele]==true){
+                ans = false; // false means  there is a cycle
+                return;
+            }
+            if(vis[ele]==false) dfs(ele,adj,vis,path);
+        }
+        path[i] = false;
+
+    }
     public boolean canFinish(int n, int[][] pre) {
+        ans = true; // true means no cycle
         List<List<Integer>> adj = new ArrayList<>();
         for(int i =1; i<=n; i++) adj.add(new ArrayList<>());
-        int [] indegree = new int[n];
         for(int i =0; i<pre.length; i++){
             int a = pre[i][0] , b = pre[i][1]; // b->a edge
             adj.get(b).add(a);
-            indegree[a]++;
         }
-        // kahn's algorithm
-        Queue<Integer> q = new LinkedList<>();
-        List<Integer> ans = new ArrayList<>();
+        boolean[] vis = new boolean[n];
+        boolean[] path = new boolean[n];
         for(int i =0; i<n; i++){
-            if(indegree[i]==0) q.add(i);
+            if(vis[i]==false) dfs(i,adj,vis,path);
         }
-        while(q.size()>0){
-            int front = q.remove();
-            ans.add(front);
-            for(int ele : adj.get(front)){
-                indegree[ele]--;
-                if(indegree[ele]==0){
-                    q.add(ele); 
-                    
-            }
-        }
-        
-    }
-     return (ans.size()==n);
+        return ans;
+       
     }
 }
 
