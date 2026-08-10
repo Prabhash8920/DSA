38755c573bd8bd2ba0e73d70d346ac5458a68e70
Prabhash kumar <Prabhashkumar7835@gmail.com>
2026-08-10 17:39:23 +0530

    Time: 15 ms (64.49%) | Memory: 42.6 MB (60.84%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -1,28 +1,19 @@
+//Approach-2 (Bottom Up)
+//T.C : O(n*sqrt(n))
+//S.C : O(n)
 class Solution {
-    int[] t;
-
-    private boolean solve(int n) {
-        if(n == 0) {
-            return false;
-        }
-        if(t[n] != -1) {
-            return t[n] == 1 ? true : false;
-        }
-        for(int k = 1; k*k <= n; k++) { //O(sqrt(n))
-            
-            if(solve(n - (k*k)) == false) { //Call for Bob //False - Bob lost it
-                //Alice won the game
-                t[n] = 1;
-                return true;
+    public boolean winnerSquareGame(int n) {
+        boolean[] t = new boolean[n+1]; //defaults to all false
+        //Base case . n == 0, return false
+        t[0] = false; //base case
+        for(int i = 1; i < n+1; i++) {
+            for(int k = 1; k*k <= i; k++) {
+                if(t[i - (k*k)] == false) {
+                    t[i] = true;
+                    break;
+                }
             }
         }
-        t[n] = 0; //Alice could never win. Lost it.
-        return false;
-    }
-
-    public boolean winnerSquareGame(int n) {
-           t = new int[n+1];
-        Arrays.fill(t, -1);
-        return solve(n);
+        return t[n]; //return solve(n)
     }
 }
