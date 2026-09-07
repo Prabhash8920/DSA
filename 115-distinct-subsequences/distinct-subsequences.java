f2ebf874f254aa7ad8c9e4fecb8ebe8396ac9a29
Prabhash kumar <Prabhashkumar7835@gmail.com>
2026-09-07 21:56:42 +0530

    Time: 53 ms (5.16%) | Memory: 138.1 MB (6.33%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,38 +1,26 @@
 class Solution {
-
-    public long solve(int i, int j, String s, String t, long[][] dp) {
-
-        if (j < 0)
-            return 1;
-
-        if (i < 0)
-            return 0;
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        if (s.charAt(i) == t.charAt(j)) {
-
-            long pick = solve(i - 1, j - 1, s, t, dp);
-            long skip = solve(i - 1, j, s, t, dp);
-
-            return dp[i][j] = pick + skip;
-        }
-
-        return dp[i][j] = solve(i - 1, j, s, t, dp);
+    int[][] dp = new int[1001][1001];
+    int solve(String s, String t, int m, int n) {
+        if(n == 0)
+            return dp[m][n] = 1;
+        if(m == 0)
+            return dp[m][n] = 0;
+        
+        if(dp[m][n] != -1)
+            return dp[m][n];
+        
+        if(s.charAt(m-1) == t.charAt(n-1))
+            return dp[m][n] = solve(s, t, m-1, n) + solve(s, t, m-1, n-1);
+        else
+            return dp[m][n] = solve(s, t, m-1, n);
     }
-
     public int numDistinct(String s, String t) {
-
         int m = s.length();
         int n = t.length();
-
-        long[][] dp = new long[m][n];
-
-        for (int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-
-        return (int) solve(m - 1, n - 1, s, t, dp);
+        if(m < n)
+            return 0;
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
+        return solve(s, t, m, n);
     }
 }
